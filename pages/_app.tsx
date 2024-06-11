d336f3e97c43f641c065e0c241ec6797be5e3022
shou1027 <longword871@gmail.com>
2024-06-11 20:24:15 +0900

    muiお試し

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import '@/styles/globals.css';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import type { AppProps } from 'next/app';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#eba536',
+    },
+    secondary: {
+      main: '#d6d62b',
+    },
+  },
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />;
+    </ThemeProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,124 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
+import {
+  Button,
+  LinearProgress,
+  Slider,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { useState } from 'react';
+import { Delete, Settings, VolumeDown, VolumeUp } from '@mui/icons-material';
+import { Link } from '@/components/atoms/Link';
+import { PolarAngleAxis, PolarGrid, Radar, RadarChart } from 'recharts';
 
 export default function Home() {
+  const [slider, setSlider] = useState(0);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div>
+      <TextField label="outlined" variant="outlined" type="number" />
+      <Stack
+        spacing={10}
+        justifyContent="center"
+        direction="row"
+        sx={{ marginTop: 12.1 }}
+      >
+        <Button variant="outlined">aaiiuu</Button>
+      </Stack>
+      <Stack
+        spacing={2}
+        direction="row"
+        sx={{ mb: 1, maxWidth: 400, mx: 'auto' }}
+        justifyContent="center"
+      >
+        <VolumeDown />
+        <Slider
+          aria-label="Volume"
+          value={slider}
+          max={100}
+          min={0}
+          onChange={(_event, value) => setSlider(value as number)}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <VolumeUp />
+      </Stack>
+      <LinearProgress
+        variant="determinate"
+        value={slider}
+        sx={{ '.MuiLinearProgress-bar': { transition: 20 } }}
+      />
+      <Typography variant="body1" color="red" fontSize={30}>
+        {slider}
+      </Typography>
+      <Stack direction="row" spacing={1}>
+        <Button variant="contained" color="error">
+          error
+        </Button>
+        <Button variant="contained" color="info">
+          info
+        </Button>
+        <Button variant="contained" color="inherit" endIcon={<Delete />}>
+          inherit
+        </Button>
+        <Button variant="contained" color="primary">
+          primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          secondary
+        </Button>
+        <Button variant="contained" color="success">
+          success
+        </Button>
+        <Button
+          variant="contained"
+          color="warning"
+          startIcon={<Settings color="info" />}
+          endIcon={<Delete />}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          warning
+        </Button>
+      </Stack>
+      <RadarChart
+        height={250}
+        width={500}
+        data={[
+          {
+            subject: '炭水化物',
+            A: slider * 0.8 + 20,
+            fullMark: 100,
+          },
+          {
+            subject: 'タンパク質',
+            A: 100 - slider * 0.8,
+            fullMark: 100,
+          },
+          {
+            subject: '脂質',
+            A: slider * 0.5 + 50,
+            fullMark: 100,
+          },
+          {
+            subject: 'ビタミン',
+            A: 100 - slider * 0.5,
+            fullMark: 100,
+          },
+          {
+            subject: 'ミネラル',
+            A: 85,
+            fullMark: 100,
+          },
+        ]}
+      >
+        <PolarGrid />
+        <PolarAngleAxis dataKey="subject" />
+        {/* <PolarRadiusAxis angle={30} domain={[0, 100]} /> */}
+        <Radar
+          name="Mike"
+          dataKey="A"
+          stroke="#8884d8"
+          fill="#8884d8"
+          fillOpacity={0.5}
+        />
+      </RadarChart>
+    </div>
   );
 }
